df6a9ff49fb84bc82f8d966367a749906c1e5456
Tracie <mac.trace89@gmail.com>
2016-06-28 18:02:38 -0700

    action bar not working

--- app/src/main/java/com/mclaughlin/tracie/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/mclaughlin/tracie/criminalintent/CrimeFragment.java
@@ -1,7 +1,11 @@
 package com.mclaughlin.tracie.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -12,6 +16,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -20,25 +25,37 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
     public static final String EXTRA_CRIME_ID = "com.mclaughlin.tracie.criminalintent.crime_id";
 
+    private static final String DIALOG_DATE = "date";
+    private static final int REQUEST_DATE = 0;
+
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
 
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
+    }
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
+        UUID crimeId = (UUID) getArguments().getSerializable(EXTRA_CRIME_ID);
 
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 
-        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(
@@ -46,23 +63,34 @@ public class CrimeFragment extends Fragment {
                 mCrime.setTitle(c.toString());
 
             }
+
             public void beforeTextChanged(
                     CharSequence c, int start, int count, int after) {
 
             }
+
             public void afterTextChanged(Editable c) {
 
             }
         });
 
-        mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        updateDate();
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FragmentManager fm = getActivity()
+                        .getSupportFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mCrime.getDate());
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                dialog.show(fm, DIALOG_DATE);
+            }
+        });
 
-        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
-        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mCrime.setSolved(isChecked);
             }
         });
@@ -70,7 +98,7 @@ public class CrimeFragment extends Fragment {
         return v;
     }
 
-    public static CrimeFragment newInstance(UUID crimeId){
+    public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
         args.putSerializable(EXTRA_CRIME_ID, crimeId);
 
@@ -79,5 +107,17 @@ public class CrimeFragment extends Fragment {
 
         return fragment;
     }
-}
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+
+
+    }
+}
--- app/src/main/java/com/mclaughlin/tracie/criminalintent/CrimeLab.java
+++ app/src/main/java/com/mclaughlin/tracie/criminalintent/CrimeLab.java
@@ -19,12 +19,14 @@ public class CrimeLab {
     private CrimeLab(Context appContext){
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
-        for (int i = 0; i < 100; i++){
-            Crime c = new Crime();
-            c.setTitle("Crime #" +i);
-            c.setSolved(i % 2 == 0); // every other one
-            mCrimes.add(c);
-        }
+        //Generates 100 random crimes
+//        for (int i = 0; i < 100; i++){
+//            Crime c = new Crime();
+//            c.setTitle("Crime #" +i);
+//            c.setSolved(i % 2 == 0); // every other one
+//            mCrimes.add(c);
+//        }
+        // NO LONGER NECESSARY REFER TO addCrime method
     }
 
     public static CrimeLab get(Context c){
@@ -32,6 +34,10 @@ public class CrimeLab {
             sCrimeLab = new CrimeLab(c.getApplicationContext());
         }
         return sCrimeLab;
+    }
+
+    public void addCrime(Crime c){
+        mCrimes.add(c);
     }
     public ArrayList<Crime> getCrimes(){
         return mCrimes;
